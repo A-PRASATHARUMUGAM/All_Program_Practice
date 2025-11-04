2b0276d920764cf86e84258807a6656eeeda0919
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-11-04 14:51:21 +0530

    Practice the functional component in react

--- React_js/practice_react/src/App.js
+++ React_js/practice_react/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+
+  function handlefunction(){
+      let arr=['Earn','Grow','Give'];
+
+   
+      let rand = Math.floor(Math.random()*3);
+
+      return arr[rand];
+
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+   <div>
+      <p> Hello World </p> 
+      <p> Let's {handlefunction()} Money </p>
+   </div>
   );
 }
 
