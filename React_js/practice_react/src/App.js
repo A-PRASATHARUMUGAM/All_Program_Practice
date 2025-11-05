4833d2611bcea79102fafdfb09bc3df7150f95b6
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-11-05 10:31:19 +0530

    create the new component

--- React_js/practice_react/src/App.js
+++ React_js/practice_react/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from './Header';
 import Section from './Section';
@@ -6,11 +5,12 @@ import Footer from './Footer';
 import RectCss from "./React_Applying_CSS";
 import ReactEvents from "./React_Click_Events";
 import ReactUseState from "./React_useState_Hook";
+import ReactListKeys from "./React_List_and_Keys";
 
 function App() {
 
 
-  return (
+  return ( 
 
    <div>
 
@@ -20,6 +20,7 @@ function App() {
       <RectCss/>
       <ReactEvents />
       <ReactUseState /> 
+      <ReactListKeys />
    </div>
   );
 }
