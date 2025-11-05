aec3f40208f11f87f7694213d0a5ed863e6035fb
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-11-05 10:32:05 +0530

    create the new component

---
+++ React_js/practice_react/src/React_List_and_Keys.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const ReactListKeys=() =>{
+
+    return (
+
+        <div>
+            <h1 style={{textAlign:'center',color:'red'}}>React List and Keys </h1>
+
+
+
+
+        </div>
+    );
+
+
+
+}
+
+
+export default ReactListKeys;
+
